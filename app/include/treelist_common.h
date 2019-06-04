a5d8358e5a39d655b1e3e5419eed47ecb3760154
Shaco-Ma <18588261041@126.com>
2019-06-04 19:35:28 +0800

    upload file

---
+++ app/include/treelist_common.h
@@ -0,0 +1,24 @@
+#ifndef _TREELIST_COMMON_H_
+#define _TREELIST_COMMON_H_
+
+typedef enum
+{
+	//表示前驱
+	TREELIST_MAX,
+	//表示后继
+	TREELIST_MIN,
+}TREELIST_MAIN_MIN_NUM;
+
+typedef enum
+{
+	//表示前驱
+	TREELIST_PRE,
+	//表示后继
+	TREELIST_POST,
+}TREELIST_ENUM;
+
+#endif
+
+
+
+
